eade5ab80eaa617b56fb37478eaf0d34e37da2d5
Rob Hilton <rhilton@weber.edu>
2014-10-20 16:59:52 -0600

    commit

---
+++ OrderDetail/Controllers/OrderController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrderDetail.Models;
+using System.Data.SqlClient;
+
+namespace OrderDetail.Controllers
+{
+    public class OrderController : Controller
+    {
+        private OrderEntryDbContext db = new OrderEntryDbContext();
+
+        //
+        // GET: /Order/
+
+        public ActionResult Index()
+        {
+            return View(db.orders.ToList());
+        }
+
+        //
+        // GET: /Order/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            OrderTbl ordertbl = db.orders.Find(id);
+            if (ordertbl == null)
+            {
+                return HttpNotFound();
+            }
+
+            List<OrderDetail.Models.OrderDetail> details =
+                db.orderDetails.SqlQuery(
+                    "SELECT OrdLineId, l.ProdNo, p.ProdName, l.Qty, p.ProdPrice " +
+                    "FROM OrderTbl o, OrdLine l, Product p " +
+                    "WHERE o.OrdNo = l.OrdNo " +
+                    "  AND l.ProdNo = p.ProdNo " +
+                    "  AND o.OrdId = @OrdId "
+                    , new SqlParameter("OrdId", id)).ToList();
+
+            ViewBag.orderDetails = details;
+
+            return View(ordertbl);
+
+        }
+
+        //
+        // GET: /Order/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Order/Create
+
+        [HttpPost]
+        public ActionResult Create(OrderTbl ordertbl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.orders.Add(ordertbl);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ordertbl);
+        }
+
+        //
+        // GET: /Order/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            OrderTbl ordertbl = db.orders.Find(id);
+            if (ordertbl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ordertbl);
+        }
+
+        //
+        // POST: /Order/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(OrderTbl ordertbl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ordertbl).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ordertbl);
+        }
+
+        //
+        // GET: /Order/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            OrderTbl ordertbl = db.orders.Find(id);
+            if (ordertbl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ordertbl);
+        }
+
+        //
+        // POST: /Order/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OrderTbl ordertbl = db.orders.Find(id);
+            db.orders.Remove(ordertbl);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OrderDetail/Models/OrderEntryData.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace OrderDetail.Models
+{
+    public class OrderEntryData
+    {
+    }
+
+    [Table("Customer")]
+    public class Customer
+    {
+        [Key]
+        [Column("CustId")]
+        public int custId{get; set;}
+        public string custNo { get; set; }
+        public string custFirstName { get; set; }
+        public string custLastName { get; set; }
+        public string custStreet { get; set; }
+        public string custCity { get; set; }
+        public string custState { get; set; }
+        public string custZip { get; set; }
+        public decimal custBal { get; set; }
+    }
+
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int EmpId       {get; set;}
+        public string EmpNo     {get; set;}
+        public string EmpFirstName {get; set;}
+        public string EmpLastName {get; set;}
+        public string EmpPhone     {get; set;}
+        public string SupEmpNo     {get; set;}
+        public decimal EmpCommRate  {get; set;}
+        public string EmpEmail    {get; set;}
+    }
+
+    [Table("OrderTbl")]
+    public class OrderTbl
+    {
+        [Key]
+        public int OrdId { get; set;}
+        public string OrdNo {get; set;}
+        public DateTime  OrdDate {get; set;}
+        public string CustNo {get; set;}
+        public string EmpNo {get; set;}
+        public string OrdName {get; set;}
+        public string OrdStreet {get; set;}
+        public string OrdCity {get; set;}
+        public string OrdState {get; set;}
+        public string OrdZip {get; set;}
+    }
+
+    [Table("OrdLine")]
+    public class OrdLine
+    {
+        [Key]
+        public int OrdLineId {get; set;}
+        public string OrdNo {get; set;}
+        public string ProdNo {get; set;}
+        public int Qty {get; set;}
+    }
+
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public int ProdId {get; set;}
+        public string ProdNo {get; set;}
+        public string ProdName {get; set;}
+        public string ProdMfg {get; set;}
+        public int ProdQOH {get; set;}
+        public decimal ProdPrice  {get; set;}
+        public DateTime? ProdNextShipDate {get; set;}
+    }
+
+    public class OrderEntryDbContext : DbContext
+    {
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<OrderTbl> orders { get; set; }
+        public DbSet<OrdLine> orderLines { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<OrderDetail> orderDetails { get; set; }
+    }
+
+    public class OrderDetail
+    {
+        [Key]
+        public int? OrdLineId { get; set; }
+        public string ProdNo { get; set; }
+        public string ProdName { get; set; }
+        public int Qty { get; set; }
+        public decimal ProdPrice { get; set; }
+    }
+
+    public class OrderDetails
+    {
+        public List<OrderDetail> orderDetails { get; set; }
+    }
+}
